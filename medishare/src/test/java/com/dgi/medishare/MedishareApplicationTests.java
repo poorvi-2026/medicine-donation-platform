dd01e0f9db622606837d2ff2b701c35f5b8a4fef
Poorvi <poorviparashar2004@gmail.com>
2026-07-22 22:14:14 +0530

    Initial Spring Boot project setup with dependencies

---
+++ medishare/src/test/java/com/dgi/medishare/MedishareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgi.medishare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedishareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
